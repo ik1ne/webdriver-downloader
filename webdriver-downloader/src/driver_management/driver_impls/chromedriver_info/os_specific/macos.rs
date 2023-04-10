e1e443281336aa46ddb1aef6c5a10e9b01741152
ik1ne <3996272+ik1ne@users.noreply.github.com>
2023-04-10 22:31:17 +0900

    Change: add windows, macos for runner os. (#25)
    * Change: add windows, macos for runner os.
    * Fix: support for Macos x86_64 architecture.

--- webdriver-downloader/src/driver_management/driver_impls/chromedriver_info/os_specific/macos.rs
+++ webdriver-downloader/src/driver_management/driver_impls/chromedriver_info/os_specific/macos.rs
@@ -1,9 +1,22 @@
+#[cfg(target_arch = "aarch64")]
 pub const ZIPFILE_NAME_RE: &str = r#"<Key>([0-9.]*?)/chromedriver_mac_arm64.zip</Key>"#;
+#[cfg(target_arch = "x86_64")]
+pub const ZIPFILE_NAME_RE: &str = r#"<Key>([0-9.]*?)/chromedriver_mac64.zip</Key>"#;
+
 pub const DRIVER_NAME_IN_ARCHIVE: &str = "chromedriver";
 
+#[cfg(target_arch = "aarch64")]
 pub fn build_url(version_string: &str) -> String {
     format!(
         "https://chromedriver.storage.googleapis.com/{}/chromedriver_mac_arm64.zip",
+        version_string
+    )
+}
+
+#[cfg(target_arch = "x86_64")]
+pub fn build_url(version_string: &str) -> String {
+    format!(
+        "https://chromedriver.storage.googleapis.com/{}/chromedriver_mac64.zip",
         version_string
     )
 }
--- webdriver-downloader/src/driver_management/driver_impls/geckodriver_info/os_specific/macos.rs
+++ webdriver-downloader/src/driver_management/driver_impls/geckodriver_info/os_specific/macos.rs
@@ -1,8 +1,17 @@
 pub const DRIVER_NAME_IN_ARCHIVE: &str = "geckodriver";
 
+#[cfg(target_arch = "aarch64")]
 pub fn build_url(version_string: &str) -> String {
     format!(
         "https://github.com/mozilla/geckodriver/releases/download/v{ver}/geckodriver-v{ver}-macos-aarch64.tar.gz",
+        ver=version_string
+    )
+}
+
+#[cfg(target_arch = "x86_64")]
+pub fn build_url(version_string: &str) -> String {
+    format!(
+        "https://github.com/mozilla/geckodriver/releases/download/v{ver}/geckodriver-v{ver}-macos.tar.gz",
         ver=version_string
     )
 }
