181b2b66a94566f5c461898ca384352a1d3ee854
ik1ne <ik1ne@naver.com>
2023-04-19 21:33:43 +0900

    Add: WebdriverDownloadInfo::is_installed.

--- webdriver-downloader-cli/src/cli/run.rs
+++ webdriver-downloader-cli/src/cli/run.rs
@@ -1,10 +1,12 @@
-use webdriver_downloader::{ChromedriverInfo, GeckodriverInfo, WebdriverDownloadInfo};
+use webdriver_downloader::{
+    ChromedriverInfo, GeckodriverInfo, WebdriverDownloadError, WebdriverDownloadInfo,
+};
 
 use super::build_arg::*;
 use super::check_arg::*;
 use super::*;
 
-pub async fn run() -> anyhow::Result<()> {
+pub async fn run() -> anyhow::Result<String> {
     let args = get_args()?;
 
     check_driver_install_path(&args.driver_install_path, args.mkdir)?;
@@ -14,14 +16,28 @@ pub async fn run() -> anyhow::Result<()> {
         DriverType::Chrome => {
             let driver_info = ChromedriverInfo::new(args.driver_install_path, args.browser_path);
 
-            driver_info.download_verify_install(5).await?;
-            Ok(())
+            install_if_needed(&driver_info, 5)
+                .await
+                .map_err(|e| e.into())
         }
         DriverType::Gecko => {
             let driver_info = GeckodriverInfo::new(args.driver_install_path, args.browser_path);
 
-            driver_info.download_verify_install(5).await?;
-            Ok(())
+            install_if_needed(&driver_info, 5)
+                .await
+                .map_err(|e| e.into())
         }
+    }
+}
+
+async fn install_if_needed(
+    driver_info: &impl WebdriverDownloadInfo,
+    max_tries: usize,
+) -> Result<String, WebdriverDownloadError> {
+    if driver_info.is_installed().await {
+        Ok("Driver already installed.".to_string())
+    } else {
+        driver_info.download_verify_install(max_tries).await?;
+        Ok("Driver installed successfully.".to_string())
     }
 }
