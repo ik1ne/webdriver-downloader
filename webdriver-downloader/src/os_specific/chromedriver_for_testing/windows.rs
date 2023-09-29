17e34410e4398990c8f105cb24654ee66f7294d3
ik1ne <ik1ne@naver.com>
2023-09-29 09:39:35 +0900

    fix linux and windows build failure

--- webdriver-downloader/src/os_specific/chromedriver_for_testing/windows.rs
+++ webdriver-downloader/src/os_specific/chromedriver_for_testing/windows.rs
@@ -1,10 +1,6 @@
-use std::ffi::OsString;
-use std::path::{Path, PathBuf};
-
-use semver::Version;
+use std::path::PathBuf;
 
 use crate::os_specific::DefaultPathError;
-use crate::traits::version_req_url_info::VersionReqError;
 
 #[cfg(target_arch = "x86")]
 pub const PLATFORM: &str = "win32";
@@ -12,23 +8,12 @@ pub const PLATFORM: &str = "win32";
 #[cfg(target_arch = "x86_64")]
 pub const PLATFORM: &str = "win64";
 
+pub const DRIVER_EXECUTABLE_NAME: &str = "chromedriver";
+
 pub fn default_browser_path() -> Result<PathBuf, DefaultPathError> {
     let program_files = std::env::var("ProgramFiles")?;
     Ok(PathBuf::from(format!(
         r"{}\Google\Chrome\Application\chrome.exe",
         program_files
     )))
-}
-
-pub fn binary_version(browser_path: &Path) -> Result<Version, VersionReqError> {
-    let mut child = std::process::Command::new("powershell");
-
-    let mut command = OsString::from("(Get-Item \"");
-    command.push(browser_path);
-    command.push("\").VersionInfo.FileVersion");
-
-    child.arg("-command").arg(command);
-
-    let output = child.output()?;
-    lenient_semver::parse(&String::from_utf8_lossy(&output.stdout)).map_err(|e| e.owned().into())
 }
